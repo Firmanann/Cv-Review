67eb76c191d91c115d1286a632c7885fb7af28bd
Firmanann <firmanardiyan02@gmail.com>
2026-08-18 20:58:32 +0700

    chore(be/infra): initialize spring boot project and docker setup

---
+++ cvreview-backend/src/test/java/com/mann/cvreview/CvreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mann.cvreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
